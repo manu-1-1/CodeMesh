607d16f381651ebaa5bc13749b186eb02cf4ce6f
Manukrishna J P <themanu110206@gmail.com>
2026-06-22 17:52:22 +0530

    Created github.js inside routes

---
+++ backend/src/routes/github.js
@@ -0,0 +1,9 @@
+import express from 'express';
+import { prisma } from '../lib/prisma.js';
+import { authenticateToken } from '../middleware/auth.js';
+
+const router = express.Router();
+router.use(authenticateToken);
+
+
+export default router;
